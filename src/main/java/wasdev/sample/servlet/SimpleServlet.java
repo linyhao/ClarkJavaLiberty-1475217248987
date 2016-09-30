5a22e8229affd4ba6017fdda52b8a44922f528f5
LinYingHao <LinYingHao@ADMINIB-RQB3IVU>
2016-09-30 15:50:07 +0800

    1st POC of Watson API Translation

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,6 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
+
+import com.ibm.watson.developer_cloud.language_translation.v2.model.*;
+import com.ibm.watson.developer_cloud.language_translation.v2.*;
+
+import com.ibm.watson.developer_cloud.conversation.v1.*;
+import com.ibm.watson.developer_cloud.conversation.v1.model.*;
+import com.ibm.watson.developer_cloud.conversation.v1_experimental.model.*;
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+
 /**
  * Servlet implementation class SimpleServlet
  */
@@ -23,5 +33,18 @@ public class SimpleServlet extends HttpServlet {
         response.setContentType("text/html");
         response.getWriter().print("Hello World!");
     }
-
+    
+    private static LanguageTranslation service = new LanguageTranslation();
+    
+    public static void main(String[] args) {
+    	service.setUsernameAndPassword("d02a80d2-fb2a-4941-9d74-7ed0e72541c4", "i6YdoXRKCWEz");
+    	TranslationResult result = service.translate("hello", Language.ENGLISH, Language.SPANISH).execute();
+    	System.out.println(result);
+    }
+    
+    /*private static ConversationService service = new ConversationService("2016-09-30");
+    
+    public static void main(String[] args) {
+    	service
+    }*/
 }
