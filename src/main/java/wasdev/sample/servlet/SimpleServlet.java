06583baa61ee226cc6b5ffe55c95e1336c7d1fc9
Clark Lin <linyhao@cn.ibm.com>
2016-10-01 20:48:11 -0500

    Translation POC V8

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -35,7 +35,7 @@ public class SimpleServlet extends HttpServlet {
         //response.getWriter().print("result is " + result.toString());
         //System.out.println("result is " + result);
         response.setContentType("application/json");
-        response.getWriter().print("This is test of modification 2");
+        response.getWriter().print(result);
     }
     
     private static LanguageTranslation service = new LanguageTranslation();
