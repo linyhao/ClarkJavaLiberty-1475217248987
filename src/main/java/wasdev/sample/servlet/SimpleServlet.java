4395cdbaf6f2171c55dac59b4f47949ec1fc8450
Clark Lin <linyhao@cn.ibm.com>
2016-10-05 07:16:57 -0500

    Translation POC V12

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -35,7 +35,7 @@ public class SimpleServlet extends HttpServlet {
         //response.getWriter().print("result is " + result.toString());
         //System.out.println("result is " + result);
         response.setContentType("application/json");
-        response.getWriter().print(result);
+        response.getWriter().print("V12 "result);
     }
     
     private static LanguageTranslation service = new LanguageTranslation();
