1aed77ef8ef3143b1a1a16672e0b7bbadbb14e12
LinYingHao <LinYingHao@ADMINIB-RQB3IVU>
2016-10-06 09:53:27 +0800

    Development Branch Test V1

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -35,7 +35,7 @@ public class SimpleServlet extends HttpServlet {
         //response.getWriter().print("result is " + result.toString());
         //System.out.println("result is " + result);
         response.setContentType("application/json");
-        response.getWriter().print("V17 Dian Dian " + result.toString());
+        response.getWriter().print("Development Branch Test V1" + result.toString());
     }
     
     private static LanguageTranslation service = new LanguageTranslation();
